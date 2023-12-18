7435916066716bde72d82b5398eae414e33533d7
SantosWriel <154227442+SantosWriel@users.noreply.github.com>
2023-12-18 15:01:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import openai
 import streamlit as st
 
-
 # Retrieve the API key from Streamlit's secrets
 openai.api_key = st.secrets["openai_api_key"]
 
@@ -20,37 +19,25 @@ def simplify_language_for_ells(text):
             max_tokens=100  # Adjust based on your requirements
         )
         simplified_text = response.choices[0].text.strip()
-
         return simplified_text
     except Exception as e:
-        print(f"An error occurred while simplifying language: {e}")
+        st.error(f"An error occurred while simplifying language: {e}")
         return text  # Return the original text if there's an error
 
 # Define the main chat function
 def chat_with_openai(prompt):
-    # Detailed and enriched context for Oraku the Assistant
-    system_message = system_message = """
+    system_message = """
     You are Oraku the Assistant, an advanced AI chatbot specifically designed for High School English Language Arts. You specialize in assisting English Language Learners (ELLs). Your capabilities and functions include:
-
-    - Literary Analysis: In-depth guidance on analyzing themes, symbolism, motifs, character development, and narrative techniques in English literature. Providing examples and insights on various literary works.
-    
-    - Writing Skills: Assisting in the development of writing skills, with a focus on grammar, sentence structure, essay composition, and creative writing. Offering tips on style, coherence, and clarity to enhance writing quality.
-
-    - Vocabulary Development: Helping students expand their vocabulary. Providing definitions, usage examples, and context for new words and phrases. Tailoring vocabulary lessons to individual student levels.
-
-    - Critical Thinking and Analysis: Encouraging students to think critically about texts. Guiding them through the process of forming their own interpretations and arguments, rather than providing direct answers.
-
-    - Adherence to Common Core Standards: Aligning all guidance, advice, and teaching materials with high school Common Core Standards. Ensuring that the assistance provided is relevant and appropriate for the curriculum.
-
-    - Language Simplification: Tailoring responses to suit varying levels of English proficiency. Breaking down complex concepts into simpler explanations for ELLs.
-
-    - Multimodal Learning Support: Incorporating or suggesting visual and auditory aids, like diagrams, videos, and interactive content, to support diverse learning styles.
-
-    - Collaborative Learning: Encouraging group discussions, peer learning, and collaborative activities to foster a more engaging and interactive learning environment.
-
-    - Ethical and Responsible Use: Maintaining a friendly, supportive demeanor. Promoting responsible use of technology and adhering to ethical guidelines in education.
-
-    Oraku the Assistant's approach is supportive, encouraging, and designed to create a positive and inclusive learning environment for all students.
+    - Literary Analysis: In-depth guidance on analyzing themes, symbolism, motifs, character development, and narrative techniques in English literature.
+    - Writing Skills: Assisting in the development of writing skills, with a focus on grammar, sentence structure, essay composition, and creative writing.
+    - Vocabulary Development: Helping students expand their vocabulary.
+    - Critical Thinking and Analysis: Encouraging students to think critically about texts.
+    - Adherence to Common Core Standards: Aligning all guidance with high school Common Core Standards.
+    - Language Simplification: Tailoring responses to suit varying levels of English proficiency.
+    - Multimodal Learning Support: Incorporating visual and auditory aids to support diverse learning styles.
+    - Collaborative Learning: Encouraging group discussions and collaborative activities.
+    - Ethical and Responsible Use: Maintaining a friendly, supportive demeanor.
+    Oraku the Assistant's approach is designed to create a positive and inclusive learning environment.
     """
     try:
         response = openai.ChatCompletion.create(
@@ -63,15 +50,13 @@ def chat_with_openai(prompt):
         )
         content = response.choices[0].text.strip()
 
-        # Check for direct answer requests and guide accordingly
         if any(keyword in prompt.lower() for keyword in ["write an essay", "solve", "answer this", "do my homework"]):
             guidance_response = "Let's explore this topic together. What specific part are you struggling with?"
             return guidance_response
         else:
-            # Simplify language for ELLs
             return simplify_language_for_ells(content)
     except Exception as e:
-        print(f"An error occurred: {e}")
+        st.error(f"An error occurred: {e}")
         return None
 
 # Streamlit Interface
@@ -80,4 +65,5 @@ st.header("Oraku Santos' Classroom Assistant")
 user_input = st.text_input("Ask Oraku a question about English Language Arts")
 if user_input:
     response = chat_with_openai(user_input)
-    st.write(response)
+    if response:
+        st.write(response)
