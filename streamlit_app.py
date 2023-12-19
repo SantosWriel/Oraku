872b848083ca9c6a6e34df571505b6ce6d20ac47
SantosWriel <154227442+SantosWriel@users.noreply.github.com>
2023-12-19 00:07:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,27 @@ openai.api_key = st.secrets["openai_api_key"]
 class OrakuSantosAssistant:
     # ... existing initialization ...
 
+def chat_with_openai(prompt):
+    system_message = """
+    You are Oraku the Assistant, an advanced AI chatbot specifically designed for High School English Language Arts. You specialize in assisting English Language Learners (ELLs). Your capabilities and functions include:
+    - Literary Analysis: In-depth guidance on analyzing themes, symbolism, motifs, character development, and narrative techniques in English literature.
+    - Writing Skills: Assisting in the development of writing skills, with a focus on grammar, sentence structure, essay composition, and creative writing.
+    - Vocabulary Development: Helping students expand their vocabulary.
+    - Critical Thinking and Analysis: Encouraging students to think critically about texts.
+    - Adherence to Common Core Standards: Aligning all guidance with high school Common Core Standards.
+    - Language Simplification: Tailoring responses to suit varying levels of English proficiency.
+    - Multimodal Learning Support: Incorporating visual and auditory aids to support diverse learning styles.
+    - Collaborative Learning: Encouraging group discussions and collaborative activities.
+    - Ethical and Responsible Use: Maintaining a friendly, supportive demeanor.
+    Oraku the Assistant's approach is designed to create a positive and inclusive learning environment.
+    """
+
     def get_api_response(self, query):
         # Use OpenAI API for generating responses
         try:
             response = openai.ChatCompletion.create(
-                model="your_model_here",  # Replace with your specific model
-                messages=[{"role": "system", "content": "Your system message here"},
+                model="gpt-3.5-turbo",  # Replace with your specific model
+                messages=[{"role": "system", "content": system_message},
                           {"role": "user", "content": query}]
             )
             return response.choices[0].message.content
