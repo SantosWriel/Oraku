f77f1f7322c06ecb3fb81b04e4cf0f6f5e0bcc7c
SantosWriel <154227442+SantosWriel@users.noreply.github.com>
2023-12-19 05:51:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,22 +10,35 @@ class OrakuSantosAssistant:
     # ... existing initialization ...
 
     def get_api_response(self, query):
-        # Use OpenAI API for generating responses
+        # Modify the system message to encourage critical thinking
+        system_message = """
+        You are Oraku the Assistant, designed to promote critical thinking in students. 
+        Instead of giving direct answers, guide the students by asking probing questions, 
+        presenting multiple viewpoints, or encouraging them to explore and discover answers on their own.
+        """
+
         try:
             response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",  # Replace with your specific model
-                messages=[{"role": "system", "content": "Your system message here"},
+                model="gpt-3.5-turbo",
+                messages=[{"role": "system", "content": system_message},
                           {"role": "user", "content": query}]
             )
             return response.choices[0].message.content
         except Exception as e:
             return "Error: " + str(e)
 
+    def get_api_response(self, query):
+        # Example of custom logic for critical thinking
+        if "define" in query or "what is" in query:
+            return "What do you think it means? Let's explore this together."
+        # ... rest of the method ...
+
     # ... existing methods ...
 
 # Streamlit interface
 def run_streamlit_app():
-    st.title("Oraku Santos Assistant")
+    st.title("Oraku Santos Assistant - Encouraging Critical Thinking")
+    st.write("Ask Oraku a question, and it will guide you to think critically about the answer.")
     user_input = st.text_input("Ask Oraku:")
     if user_input:
         assistant = OrakuSantosAssistant()
