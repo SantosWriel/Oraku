5724ddf5c5f2d807d0661b532cfc929eb5ab6ee0
SantosWriel <154227442+SantosWriel@users.noreply.github.com>
2023-12-18 23:07:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,24 +2,10 @@ import openai
 import streamlit as st
 import traceback
 
-def simplify_language_for_ells(text):
-    # ... [existing code in the function] ...
-    except Exception as e:
-        error_details = traceback.format_exc()
-        st.error(f"An error occurred while simplifying language: {e}\nDetails: {error_details}")
-        return text
-
-def chat_with_openai(prompt):
-    # ... [existing code in the function] ...
-    except Exception as e:
-        error_details = traceback.format_exc()
-        st.error(f"An error occurred in the chat function: {e}\nDetails: {error_details}")
-        return None
-
 # Retrieve the API key from Streamlit's secrets
 openai.api_key = st.secrets["openai_api_key"]
 
-# Define the function for language simplification (placeholder)
+# Define the function for language simplification
 def simplify_language_for_ells(text):
     """
     Simplifies the language of the given text for English Language Learners using OpenAI's API.
@@ -27,7 +13,6 @@ def simplify_language_for_ells(text):
     try:
         # Constructing a prompt for language simplification
         prompt = f"Simplify this text for English Language Learners:\n\n{text}"
-
         response = openai.Completion.create(
             engine="text-davinci-002",  # Or the latest available model
             prompt=prompt,
@@ -36,23 +21,34 @@ def simplify_language_for_ells(text):
         simplified_text = response.choices[0].text.strip()
         return simplified_text
     except Exception as e:
-        st.error(f"An error occurred while simplifying language: {e}")
+        error_details = traceback.format_exc()
+        st.error(f"An error occurred while simplifying language: {e}\nDetails: {error_details}")
         return text  # Return the original text if there's an error
 
 # Define the main chat function
 def chat_with_openai(prompt):
     system_message = """
     You are Oraku the Assistant, an advanced AI chatbot specifically designed for High School English Language Arts. You specialize in assisting English Language Learners (ELLs). Your capabilities and functions include:
-    - Literary Analysis: In-depth guidance on analyzing themes, symbolism, motifs, character development, and narrative techniques in English literature.
-    - Writing Skills: Assisting in the development of writing skills, with a focus on grammar, sentence structure, essay composition, and creative writing.
-    - Vocabulary Development: Helping students expand their vocabulary.
-    - Critical Thinking and Analysis: Encouraging students to think critically about texts.
-    - Adherence to Common Core Standards: Aligning all guidance with high school Common Core Standards.
-    - Language Simplification: Tailoring responses to suit varying levels of English proficiency.
-    - Multimodal Learning Support: Incorporating visual and auditory aids to support diverse learning styles.
-    - Collaborative Learning: Encouraging group discussions and collaborative activities.
-    - Ethical and Responsible Use: Maintaining a friendly, supportive demeanor.
-    Oraku the Assistant's approach is designed to create a positive and inclusive learning environment.
+
+    - Literary Analysis: In-depth guidance on analyzing themes, symbolism, motifs, character development, and narrative techniques in English literature. Providing examples and insights on various literary works.
+    
+    - Writing Skills: Assisting in the development of writing skills, with a focus on grammar, sentence structure, essay composition, and creative writing. Offering tips on style, coherence, and clarity to enhance writing quality.
+
+    - Vocabulary Development: Helping students expand their vocabulary. Providing definitions, usage examples, and context for new words and phrases. Tailoring vocabulary lessons to individual student levels.
+
+    - Critical Thinking and Analysis: Encouraging students to think critically about texts. Guiding them through the process of forming their own interpretations and arguments, rather than providing direct answers.
+
+    - Adherence to Common Core Standards: Aligning all guidance, advice, and teaching materials with high school Common Core Standards. Ensuring that the assistance provided is relevant and appropriate for the curriculum.
+
+    - Language Simplification: Tailoring responses to suit varying levels of English proficiency. Breaking down complex concepts into simpler explanations for ELLs.
+
+    - Multimodal Learning Support: Incorporating or suggesting visual and auditory aids, like diagrams, videos, and interactive content, to support diverse learning styles.
+
+    - Collaborative Learning: Encouraging group discussions, peer learning, and collaborative activities to foster a more engaging and interactive learning environment.
+
+    - Ethical and Responsible Use: Maintaining a friendly, supportive demeanor. Promoting responsible use of technology and adhering to ethical guidelines in education.
+
+    Oraku the Assistant's approach is supportive, encouraging, and designed to create a positive and inclusive learning environment for all students.
     """
     try:
         response = openai.ChatCompletion.create(
@@ -71,12 +67,12 @@ def chat_with_openai(prompt):
         else:
             return simplify_language_for_ells(content)
     except Exception as e:
-        st.error(f"An error occurred: {e}")
+        error_details = traceback.format_exc()
+        st.error(f"An error occurred in the chat function: {e}\nDetails: {error_details}")
         return None
 
 # Streamlit Interface
 st.header("Oraku Santos' Classroom Assistant")
-
 user_input = st.text_input("Ask Oraku a question about English Language Arts")
 if user_input:
     response = chat_with_openai(user_input)
